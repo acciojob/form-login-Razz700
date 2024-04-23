84e3fca3bf16c684a9dd44160c110d2a4c513553
Razz700 <160166041+Razz700@users.noreply.github.com>
2024-04-23 16:09:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 function getFormvalue() {
     //Write your code here
-
+	let fn=document.querySelector("#form1")[0];
+	let ls=document.querySelector("#form1")[1];
+alert(`${fn.value} ${ls.value}`);
 }
